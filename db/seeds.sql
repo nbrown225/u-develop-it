e9aeed5a70564ba35d971fdd595f47b79b47c619
nbrown225 <chbrown225@yahoo.com>
2022-06-21 14:27:56 -0700

    added sql

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO candidates (first_name, last_name, industry_connected) 
+VALUES
+    ('Gerald', 'Tabernak', 1),
+    ('Mike', 'Wazowski', 1),
+    ('Sammie', 'Ye', 0),
+    ('Tyla', 'Smith', 1),
+    ('Willie', 'Willington', 1),
+    ('Velma', 'Flinestone', 0),
+    ('Scoobert', 'Doo', 1),
+    ('Barney', 'Rubble', 1),
+    ('Ronald', 'Firbank', 1),
+    ('Virginia', 'Woolf', 1),
+    ('Piers', 'Gaveston', 0),
+    ('Charles', 'LeRoi', 1),
+    ('Katherine', 'Mansfield', 1),
+    ('Dora', 'Carrington', 0),
+    ('Edward', 'Bellamy', 0),
+    ('Montague', 'Summers', 1),
+    ('Octavia', 'Butler', 1),
+    ('Officer', 'Cop', 1),
+    ('Lutinent', 'Commander', 1)
+    ('Unica', 'Zurn', 1);
